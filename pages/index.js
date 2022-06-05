7f016a634c0243c0fd9b6965a35e588c132e6ad0
Aaryan121 <aaryan.cs01@gmail.com>
2022-06-05 12:03:12 +0545

    header added

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import Header from "../components/header/Header";
+
 export default function Home() {
-  return <div>Hello Nepson</div>;
+  return (
+    <div className={"m-auto w-4/5"}>
+      <Header />
+      Hello World
+    </div>
+  );
 }
